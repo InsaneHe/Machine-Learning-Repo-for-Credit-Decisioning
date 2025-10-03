c55f6a8e415927329b6a16b6ce3b88471bb68b40
InsaneHe <14180645+insanehe@user.noreply.gitee.com>
2025-10-03 15:30:26 +0100

    Created and uploaded initial files

--- main.py
+++ main.py
@@ -2,16 +2,24 @@
 # Package importing and warnings setting
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split, GridSearchCV, RepeatedStratifiedKFold
 from sklearn.metrics import confusion_matrix, classification_report
 import xgboost as xgb
 import warnings
 warnings.filterwarnings('ignore') # Ignore specific warnings
 
-# Data preprocessing
-
+# Data reading and data preprocessing
+data = pd.read_csv("")  # Here we need to fill in the path of the csv file we want to read
 
 # Partitioning the dataset and training set
+np.random.seed(123)  # Using random seed 123
+
+test_size = float(input("Please enter the number to set the size of testing set: "))  # Set the test_size
+train_size = float(input("Please enter the number to set the size of training set: "))  # Set the train_size
+X = data.drop(columns=["label"])
+y = data["label"]
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=test_size, stratify=y, random_state=123)
 
 
-#
+
