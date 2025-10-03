fd32898cadcb89b74a88eab38bdadaa81dc69c4b
wyq <wangyiqiaonn@outlook.com>
2025-10-04 00:26:02 +0800

    update main.py

--- models_xgboost.py
+++ models_xgboost.py
@@ -1,5 +1,45 @@
 # Defining the parameter grid
 print("Now you can give me 3 integers to set the number of estimators (trees) in this model: ")
+n_estimators_a = int(input("Please enter the 1st estimator: "))
+n_estimators_b = int(input("Please enter the 2nd estimator: "))
+n_estimators_c = int(input("Please enter the 3rd estimator: "))
+
+print("Now you can give me 3 integers to set the values of depths of trees in this model: ")
+depths_a = int(input("Please enter the 1st depth: "))
+depths_b = int(input("Please enter the 2nd depth: "))
+depths_c = int(input("Please enter the 3rd depth: "))
+
+print("Now you can give me 3 integers to set the values of learning rates in this model: ")
+learning_rate_a = int(input("Please enter the 1st learning rate: "))
+learning_rate_b = int(input("Please enter the 2nd learning rate: "))
+learning_rate_c = int(input("Please enter the 3rd learning rate: "))
+
+print("Now you can give me 3 integers to set the number of estimators (trees) in this model: ")
+n_estimators_a = int(input("Please enter the 1st estimator: "))
+n_estimators_b = int(input("Please enter the 2nd estimator: "))
+n_estimators_c = int(input("Please enter the 3rd estimator: "))
+
+param_grid = {
+    "n_estimators": [n_estimators_a, n_estimators_b, n_estimators_c],  # 3 different numbers of trees to be used in xgboost model
+    "max_depth": [depths_a, depths_b, depths_c],  # The depth of trees
+    "learning_rate": [0.01, 0.1, 0.3],  # The learning rate
+    "gamma": [0, 1, 5]  # The regularization parameters
+}
+
+# Parameter tuning
+
+
+# Model training
+
+
+
+# Model evaluation
+
+
+
+#
+# Defining the parameter grid
+print("Now you can give me 3 integers to set the number of estimators (trees) in this model: ")
 n_estimators_a = int(input("Please enter the 1st estimator: "))  # we use 100
 n_estimators_b = int(input("Please enter the 2nd estimator: "))  # we use 200
 n_estimators_c = int(input("Please enter the 3rd estimator: "))  # we use 300
