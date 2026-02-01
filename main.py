641def84ffe3240e7f652126febca7a6e24433b0
InsaneHe <134916618+InsaneHe@users.noreply.github.com>
2026-02-01 23:34:36 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,49 +6,65 @@ import os
 from pathlib import Path
 from sklearn.model_selection import train_test_split, GridSearchCV, RepeatedStratifiedKFold
 from sklearn.metrics import confusion_matrix, classification_report
+import seaborn as sns
+import matplotlib.pyplot as plt
 import xgboost as xgb
 import warnings
 warnings.filterwarnings('ignore') # Ignore specific warnings
+sns.set()
+# TODO: 以下两行先注释掉，后续运行时可以解开注释以观察效果
+# pd.options.display.max_columns = None
+# pd.options.display.max_rows = None
 
-# Data reading and data preprocessing
+# Data reading
 # Setting data path（我们可以考虑使用MySQL来导入，但在那之前先用接收用户输入的方式设定data_path）
-''' Data path to use
-data_path = \
+# Data path to use (data_path_1 == smaller dataset[3904*75], data_path_2 == bigger dataset[1048576*75])
+data_path_1 = \
     r"D:\PythonProjects\Github - Machine Learning Project for Credit Decisioning\Machine-Learning-Repo-for-Credit-Decisioning\Data\loan_data_2007_2014.csv"
-'''
+data_path_2 = \
+    r"D:\PythonProjects\Github - Machine Learning Project for Credit Decisioning\Machine-Learning-Repo-for-Credit-Decisioning\Data\Bigger\loan_data_2007_2014_bigger.csv"
+data_path_list = [data_path_1, data_path_2]
 
-raw = input("Please enter the path of the data file: ").strip()
-data_path = Path(raw.strip('"').strip("'")).resolve()   # Remove quotation marks and convert to absolute paths
+def readingData():
+    for data_path in data_path_list:  # Print all the data paths available
+        print(f"The index of the data path ({data_path}): ")
+        print(f"{data_path.index()}\n")
 
-if not data_path.is_file():
-    exit("The file does not exist, the program terminates!\n")
+    index_chosen = input("Please enter the index of the path you want to choose: ")
+    print(f"You choose {data_path_list[index_chosen]}!\n")
 
-suffix = data_path.suffix.lower()
+    while True:
+        cancel_or_not = int(input("Do you want to cancel?\n1).No\t2).Yes"))
+        if cancel_or_not == 1:
+            print("Proceed!\n")
+            break
+        elif cancel_or_not == 2:
+            print("Cancelled!\n")
+            return
+        else:
+            print("Please enter integer 1 (to PROCEED) or 2 (to CANCEL)!\n")
+            continue
 
-try:
-    if suffix == '.csv':
-        df = pd.read_csv(data_path, encoding='utf-8-sig')
+    if not data_path_list[index_chosen].is_file():  # Check if the data path is a file
+        exit("The file does not exist, the program terminates!\n")
+
+    suffix = data_path_list[index_chosen].suffix.lower()
+    if suffix in ['.csv', '.xls', '.xlsx']:
+        try:
+            if suffix == '.csv':
+                df = pd.read_csv(data_path, encoding='utf-8-sig')
+            elif suffix == '.xls' or '.xlsx':
+                df = pd.read_excel(data_path)
+
+            print("Reading data file succeed!\nThe first 5 lines are:\n")
+            print(df.head())
+        except Exception as e:
+            exit(f"Attempt of reading data file failed: {e}")
     else:
-        df = pd.read_excel(data_path)
-except Exception as e:
-    exit(f"Attempt of reading data file failed: {e}")
+        print("This program only accepts csv/xls/xlsx!\n")
 
-print("Reading data file succeed!\nThe first 5 lines are:\n")
-print(df.head())
 
-# Read the data
-data = pd.read_csv(data_path)  # Here we need to fill in the path of the csv file we want to read
-
-# Partitioning the dataset and training set
-np.random.seed(123)  # Using random seed 123
-
-test_size = float(input("Please enter the number to set the size of testing set: "))  # Set the test_size
-train_size = float(input("Please enter the number to set the size of training set: "))  # Set the train_size
-X = data.drop(columns=["label"])
-y = data["label"]
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=test_size, stratify=y, random_state=123)
-
+# Main function
 if __name__ == "__main__":
     # Here, we can enter the model that we want to use
     model_map = {1: 'Logistic Regression',
